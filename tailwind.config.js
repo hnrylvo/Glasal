5a0ff365b79ec0d2a1d837ad24a8b48a0c580125
Henry <00037322@uca.edu.sv>
2025-08-30 20:54:48 -0600

    Update: Home responsive UI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+      },
+    },
   },
   plugins: [],
 }
